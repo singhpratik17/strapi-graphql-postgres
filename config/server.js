57b751c70cbbfa1081e0c6884ea9e947c3e177ad
Pratik Singh <pratiktsingh17@gmail.com>
2021-08-30 18:53:35 +0530

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2d6c496242684ea7aeb6cb89bc0987a1'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0134515e-a1b3-4102-879d-cf28b766d6af'
+};
